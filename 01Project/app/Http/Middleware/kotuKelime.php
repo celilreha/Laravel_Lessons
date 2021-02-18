5f56411d824e64512cfc915157c0f14b6d75b0d6
celilreha <celilrehaesgice@gmail.com>
2021-02-18 20:59:14 +0300

    Database

--- 01Project/app/Http/Middleware/kotuKelime.php
+++ 01Project/app/Http/Middleware/kotuKelime.php
@@ -10,14 +10,14 @@ class kotuKelime
 
     public function handle(Request $request, Closure $next)
     {
-        $kotuKelime=["elma","armut","çilek"];
+        /*$kotuKelime=["elma","armut","çilek"];
         if (in_array($request->kullanici_adi,$kotuKelime)){
             $badword=strlen($request->kullanici_adi);
             $sansur="";
             for ($i=0;$i<$badword;$i++)
                 $sansur.="*";
             echo $sansur;die;
-        }
+        }*/
         return $next($request);
     }
 }
--- 01Project/database/migrations/2021_02_18_174819_user_table2.php
+++ 01Project/database/migrations/2021_02_18_174819_user_table2.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class UserTable2 extends Migration
 {
     /**
      * Run the migrations.
@@ -15,11 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string("name");
+            $table->string("email")->comment("email adresleri toplanıyor");
+            $table->string("password");
             $table->timestamps();
         });
     }
